bb5031c551102fd91828da40eee78f54a3402a92
Giff Song <giff.song@gmail.com>
2015-05-26 15:09:16 +0900

    Reformat

--- app/scripts/controllers/article.view.js
+++ app/scripts/controllers/article.view.js
@@ -1,16 +1,16 @@
 'use strict';
 
 angular.module('go.blog')
-  .controller('article.view', function (
-    $http,
-    $window,
-    $scope,
-    $state,
-    $log,
-    $q,
-    article
-    ) {
+.controller('article.view', function (
+  $http,
+  $window,
+  $scope,
+  $state,
+  $log,
+  $q,
+  article
+) {
 
-    $scope.article = article;
+  $scope.article = article;
 
-  });
+});
--- app/scripts/routes.js
+++ app/scripts/routes.js
@@ -1,67 +1,66 @@
 'use strict';
 
 angular
-  .module('go.blog')
-  .config(function (
+.module('go.blog')
+.config(function (
 
-    // dependencies
+  // dependencies
 
-    $stateProvider,
-    $urlRouterProvider
+  $stateProvider,
+  $urlRouterProvider
 
-    ) {
-
-
-    /////////////////////////////
-    // Redirects and Otherwise //
-    /////////////////////////////
-
-    // Use $urlRouterProvider to configure any redirects (when) and invalid urls (otherwise).
-    $urlRouterProvider
-
-      // If the url is ever invalid, e.g. '/asdf', then redirect to '/' aka the home state
-      .otherwise('/');
+) {
 
 
-    //////////////////////////
-    // State Configurations //
-    //////////////////////////
+  /////////////////////////////
+  // Redirects and Otherwise //
+  /////////////////////////////
 
-    $stateProvider
+  // Use $urlRouterProvider to configure any redirects (when) and invalid urls (otherwise).
+  $urlRouterProvider
 
-      ///////////
-      // Main //
-      ///////////
+  // If the url is ever invalid, e.g. '/asdf', then redirect to '/' aka the home state
+  .otherwise('/');
 
-      .state('main', {
-        abstract: true,
-        url: '',
-        templateUrl: 'views/main.html',
-      })
 
-      .state('main.home', {
-        url: '/',
-        templateUrl: 'views/home.html',
-        //controller: 'home',
-      })
+  //////////////////////////
+  // State Configurations //
+  //////////////////////////
 
-      .state('main.article', {
-        abstract: true,
-        template: '<div data-ui-view=""></div>',
-        url: '/articles/:article',
-         resolve: {
-          article: ['api', '$stateParams', function (api, $stateParams) {
-            return api.get('articles/' + $stateParams.article);
-          }]
-        }
-      })
+  $stateProvider
 
-      .state('main.article.view', {
-        url: '',
-        templateUrl: 'views/article.view.html',
-        controller: 'article.view'
-      })
+  ///////////
+  // Main //
+  ///////////
 
-      ;
+  .state('main', {
+    abstract: true,
+    url: '',
+    templateUrl: 'views/main.html',
+  })
 
-  });
+  .state('main.home', {
+    url: '/',
+    templateUrl: 'views/home.html',
+    //controller: 'home',
+  })
+
+  .state('main.article', {
+    abstract: true,
+    template: '<div data-ui-view=""></div>',
+    url: '/articles/:article',
+    resolve: {
+      article: ['api', '$stateParams', function (api, $stateParams) {
+        return api.get('articles/' + $stateParams.article);
+      }]
+    }
+  })
+
+  .state('main.article.view', {
+    url: '',
+    templateUrl: 'views/article.view.html',
+    controller: 'article.view'
+  })
+  ;
+
+});
