9df85a3b2145d943d3238d7b08349e5e33eed03c
Giff Song <giff.song@gmail.com>
2015-06-09 21:10:57 +0900

    Disable testing

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -36,6 +36,7 @@ module.exports = function(config) {
       'bower_components/angular-ui-router/release/angular-ui-router.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
+      '.tmp/scripts/buildconfig.js',
       "app/scripts/**/*.js",
       "test/mock/**/*.js",
       "test/spec/**/*.js"
